1a810870526406d168c03d39d45774931df89645
Daisuke <nakada0907@outlook.com>
2022-11-12 22:45:04 +0900

    Installation of input forms

--- lib/model/title.dart
+++ lib/model/title.dart
@@ -10,13 +10,46 @@ class title extends StatelessWidget {
       appBar: AppBar(
         title: const Text("title"),
       ),
-      body: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => const Input()));
-              },
-              child: const Text("send"))),
+      body: Column(
+        children: <Widget>[
+          const AnswerTimeForm(),
+          ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => const Input()
+                      )
+                  );
+                },
+                child: const Text("send")
+            )
+        ],
+      )
+    );
+
+  }
+}
+
+class AnswerTimeForm extends StatelessWidget {
+  const AnswerTimeForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a total minutes for answers and explanations',
+              ),
+                keyboardType: TextInputType.number
+            ),
+          ),
+        ]
     );
   }
+
 }
