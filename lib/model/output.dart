2d394e9ab713a376c970fb10f6fe1fd261434650
ShikataHayato <hayato.shikata.g3@gmail.com>
2022-11-13 12:03:52 +0900

    output.dartとresult.dartを作成by四方

--- lib/model/output.dart
+++ lib/model/output.dart
@@ -2,35 +2,76 @@ import 'package:benesseteam4/model/result.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+
+  int _counter = 300;
+
+  void initState() {
+    // 1. Timer.periodic : 新しい繰り返しタイマーを作成します
+    // 1秒ごとに _counterを1ずつ足していく
+    Timer.periodic(
+      // 第一引数：繰り返す間隔の時間を設定
+      const Duration(seconds: 1),
+      // 第二引数：その間隔ごとに動作させたい処理を書く
+          (Timer timer) {
+        _counter--;
+        print('$_counter');
+      },
+    );
+  }
+
 
 class Output extends StatefulWidget {
   const Output({Key? key}) : super(key: key);
-
+//継承するwidgetを指定
   @override
   State<Output> createState() => _OutputState();
 }
+
 
 class _OutputState extends State<Output> {
   @override
   Widget build(BuildContext context) {
     String? isSelectedItem;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("出題された問題"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromRadius(30),
+        child: AppBar(
+          centerTitle: true,
+          title: Text('$_counter',
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 40)),
+        ),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          // Container(
+          //   height: 100,
+          //   width: double.infinity,
+          //   color: Color(0xff4fc3f7),
+          //   child: Icon(Icons.question_answer, size: 50),
+          // ),
           Container(
-            height: 300,
-            width: double.infinity,
-            color: Color(0xff4fc3f7),
-            child: Icon(Icons.question_answer, size: 50),
+            color: Colors.lightBlueAccent.shade200,
+          width: double.infinity,
+          height: 200,
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text('(1) I ___ Tokyo yesterday',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),),
+              Text('(2) Japaneseの意味は？',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),),
+              Text('(3) prejudiceの意味は？',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),),
+              Text('(4) 高校生を英語にすると？',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 20),)
+            ],
+          ),
           ),
           Text(
             '解答を選択してください',
-            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 24),
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 24,),
           ),
+          Column(
+            children: [
           Container(
             width: 160,
             color: Colors.white,
@@ -159,6 +200,8 @@ class _OutputState extends State<Output> {
               value: isSelectedItem,
             ),
           ),
+            ],
+          ),
           Center(
               child: ElevatedButton(
                   onPressed: () {
@@ -171,3 +214,5 @@ class _OutputState extends State<Output> {
     );
   }
 }
+
+
